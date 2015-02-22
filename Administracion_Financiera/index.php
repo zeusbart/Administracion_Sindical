9786174cae3a7842c392a3429fd1fa135d8d1965
Oscuro <Oscuro@Dark>
2015-02-22 14:54:49 -0600

    agregando archivos

--- Administracion_Financiera/index.php
+++ Administracion_Financiera/index.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
@@ -11,7 +7,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+   
         ?>
     </body>
 </html>
